51f0e72ee5f54161b1c45855db534b047e9f5226
Ghasem <ghasem.elyasi@gmail.com>
2024-01-26 21:23:53 +0100

    ui implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+# RockPaperScissors
+
+This is an iOS sample app built based on [Day 25 – Milestone: Projects 1-3](https://www.hackingwithswift.com/100/swiftui/25)
+
+
+## Tools
+- Xcode: 15.2
+- iOS: 17
--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,15 +7,81 @@
 
 import SwiftUI
 
+struct MovesButton: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 50))
+    }
+}
+
+extension Button {
+    func movesButtonStyle() -> some View {
+        modifier(MovesButton())
+    }
+}
+
 struct ContentView: View {
+    let moves = ["Rock", "Paper", "Scissors"]
+    
+    @State private var currentMove = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    
+    
+    @State private var score = 0
+    //@State private var showPrompt = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.3, green: 0.3, blue: 0.55), location: 0.3),
+                .init(color: Color(red: 0.6, green: 0.7, blue: 0.0), location: 0.3)
+            ], center: .top, startRadius: 300, endRadius: 500)
+            .ignoresSafeArea()
+            
+            Spacer()
+            Spacer()
+            
+            VStack(spacing: 100) {
+                
+                VStack(spacing: 10) {
+                    Text("Your score: \(score)")
+                    Text("Move: \(currentMove)")
+                    Text("Win or Lose: \(shouldWin ? "Win" : "Lose")")
+                }
+                .font(.headline)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button("🪨") {
+                        
+                    }
+                    .movesButtonStyle()
+                    
+                    Spacer()
+                    
+                    Button("📄") {
+                        
+                    }
+                    .movesButtonStyle()
+                    
+                    Spacer()
+                    
+                    Button("✂️") {
+                        
+                    }
+                    .movesButtonStyle()
+                    
+                    Spacer()
+                }
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .padding()
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 20))
+            .padding()
+            
         }
-        .padding()
     }
 }
 
